62984d0a4cb9fd792e315757aab9fda0da3e0705
hussienalrubaye <hussien89aa@yahoo.com>
2015-12-18 01:54:16 -0500

    fit law

--- app/src/main/java/com/selling/hussienalrubaye/androidselling/ControlPanel.java
+++ app/src/main/java/com/selling/hussienalrubaye/androidselling/ControlPanel.java
@@ -1,7 +1,11 @@
 package com.selling.hussienalrubaye.androidselling;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
 
 public class ControlPanel extends AppCompatActivity {
 
@@ -12,4 +16,34 @@ public class ControlPanel extends AppCompatActivity {
         SaveSettings sv=new SaveSettings(this);
         sv.LoadData();
     }
+
+
+    SearchView searchView;
+    Menu myMenu;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.control_menu, menu);
+        myMenu=menu;
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //final Context co=this;
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//code for search
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
 }
